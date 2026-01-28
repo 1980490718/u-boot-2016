b11ad9e390bfbe2aa8198f3b78e4f217ac5f3347
Willem Lee <1980490718@qq.com>
2026-01-29 04:32:14 +0800

    Refactor ipq_api.c: Extract GPIO functions, add LED blink, fix warnings
    - Extract fdt_get_gpio_number() to eliminate duplicate LED code
    - Add led_blink series functions for enhanced LED control
    - Optimize check_button_is_press() LED logic
    - Fix compiler warnings with proper header includes
    - Resolve undefined bool, u32, u16 type issues
    
    Signed-off-by: Willem Lee <1980490718@qq.com>

--- lib/ipq_api.c
+++ lib/ipq_api.c
@@ -3,82 +3,89 @@
 #include <asm/gpio.h>
 #include <fdtdec.h>
 #include <net.h>
+#include <stdbool.h>
+#include <asm-generic/global_data.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
-void led_toggle(const char *gpio_name)
-{
-	int node, value;
+static int fdt_get_gpio_number(const char *gpio_name) {
+	int node;
 	unsigned int gpio;
 	node = fdt_path_offset(gd->fdt_blob, gpio_name);
 	if (node < 0) {
 		printf("Could not find %s node in fdt\n", gpio_name);
-		return;
+		return -1;
 	}
 	gpio = fdtdec_get_uint(gd->fdt_blob, node, "gpio", 0);
 	if (gpio < 0) {
 		printf("Could not find %s node's gpio in fdt\n", gpio_name);
+		return -1;
+	}
+	return gpio;
+}
+
+void led_toggle(const char *gpio_name) {
+	int value;
+	int gpio = fdt_get_gpio_number(gpio_name);
+	if (gpio < 0) {
 		return;
 	}
-
 	value = gpio_get_value(gpio);
 	value = !value;
 	gpio_set_value(gpio, value);
 }
 
-void led_on(const char *gpio_name)
-{
-	int node;
-	unsigned int gpio;
-	node = fdt_path_offset(gd->fdt_blob, gpio_name);
-	if (node < 0) {
-		printf("Could not find %s node in fdt\n", gpio_name);
-		return;
-	}
-	gpio = fdtdec_get_uint(gd->fdt_blob, node, "gpio", 0);
+void led_on(const char *gpio_name) {
+	int gpio = fdt_get_gpio_number(gpio_name);
 	if (gpio < 0) {
-		printf("Could not find %s node's gpio in fdt\n", gpio_name);
 		return;
 	}
-
 	gpio_set_value(gpio, 1);
 }
 
-void led_off(const char *gpio_name)
-{
-	int node;
-	unsigned int gpio;
-	node = fdt_path_offset(gd->fdt_blob, gpio_name);
-	if (node < 0) {
-		printf("Could not find %s node in fdt\n", gpio_name);
-		return;
-	}
-	gpio = fdtdec_get_uint(gd->fdt_blob, node, "gpio", 0);
+void led_off(const char *gpio_name) {
+	int gpio = fdt_get_gpio_number(gpio_name);
 	if (gpio < 0) {
-		printf("Could not find %s node's gpio in fdt\n", gpio_name);
 		return;
 	}
-
 	gpio_set_value(gpio, 0);
 }
 
-bool button_is_press(const char *gpio_name, int value)
-{
-	int node;
-	unsigned int gpio;
-	node = fdt_path_offset(gd->fdt_blob, gpio_name);
-	if (node < 0) {
-		printf("Could not find %s node in fdt\n", gpio_name);
-		return false;
-	}
-	gpio = fdtdec_get_uint(gd->fdt_blob, node, "gpio", 0);
+void led_blink(const char *gpio_name, int duration) {
+	int gpio = fdt_get_gpio_number(gpio_name);
 	if (gpio < 0) {
-		printf("Could not find %s node's gpio in fdt\n", gpio_name);
+		return;
+	}
+	/* Number of cycles = duration(ms) / (on time(500ms) + off time(500ms)) */
+	int cycles = duration / 1000;
+	int i;
+	for (i = 0; i < cycles; i++) {
+		/* LED on */
+		gpio_set_value(gpio, 1);
+		mdelay(500);
+
+		/* LED off */
+		gpio_set_value(gpio, 0);
+		mdelay(500);
+	}
+}
+
+void led_blink_then_on(const char *gpio_name, int duration) {
+	led_blink(gpio_name, duration);
+	led_on(gpio_name);
+}
+
+void led_blink_then_off(const char *gpio_name, int duration) {
+	led_blink(gpio_name, duration);
+	led_off(gpio_name);
+}
+
+bool button_is_press(const char *gpio_name, int value) {
+	int gpio = fdt_get_gpio_number(gpio_name);
+	if (gpio < 0) {
 		return false;
 	}
-
-	if(gpio_get_value(gpio) == value)
-	{
+	if(gpio_get_value(gpio) == value) {
 		mdelay(10);
 		if(gpio_get_value(gpio) == value)
 			return true;
@@ -89,25 +96,14 @@ bool button_is_press(const char *gpio_name, int value)
 		return false;
 }
 
-void check_button_is_press(void)
-{
+void check_button_is_press(void) {
 	int counter = 0;
-
-	while (button_is_press("reset_key", RESET_BUTTON_PRESSED))
-	{
-
+	while (button_is_press("reset_key", RESET_BUTTON_PRESSED)) {
 		if(counter == 0)
 			printf("Reset button is pressed for: %2d second(s) ", counter);
-
-		led_off("power_led");
-		mdelay(500);
-		led_on("power_led");
-		mdelay(500);
-
+		led_blink_then_on("power_led", 1000);
 		counter++;
-
 		printf("\b\b\b\b\b\b\b\b\b\b\b\b\b%2d second(s) ", counter);
-
 		if(counter >= 3){
 			printf("\n");
 #if defined(CONFIG_IPQ807X_AP8220)
@@ -138,14 +134,14 @@ void check_button_is_press(void)
 			break;
 		}
 	}
-
 	if (counter != 0)
 		printf("\n");
-
 	return;
 }
 
-int check_fw_type(void *address){
+int check_fw_type(void *address) {
+	typedef unsigned int u32;
+	typedef unsigned short u16;
 	u32 *sign_flas=(u32 *)(address+0x5c);
 	u16 *sign_55aa=(u16 *)(address+0x1fe);
 	u32 *sign_doodfeed=(u32 *)address;
@@ -154,21 +150,13 @@ int check_fw_type(void *address){
 	u32 *sign_elf=(u32 *)address;
 	u32 *sign_mibib=(u32 *)address;
 
-	if (*sign_flas==0x73616c46)
-		return FW_TYPE_QSDK;
-	else if (*sign_ubi==0x23494255)
-		return FW_TYPE_UBI;
-	else if (*sign_doodfeed==0xedfe0dd0)
-		return FW_TYPE_NOR;
-	else if (*sign_55aa==0xaa55)
-		return FW_TYPE_GPT;
-	else if (*sign_cdt==0x00544443)
-		return FW_TYPE_CDT;
-	else if (*sign_elf==0x464c457f)
-		return FW_TYPE_ELF;
-	else if (*sign_mibib==0xfe569fac)
-		return FW_TYPE_MIBIB;
-	else
-		return -1;
+	if (*sign_flas==0x73616c46) return FW_TYPE_QSDK;
+	else if (*sign_ubi==0x23494255) return FW_TYPE_UBI;
+	else if (*sign_doodfeed==0xedfe0dd0) return FW_TYPE_NOR;
+	else if (*sign_55aa==0xaa55) return FW_TYPE_GPT;
+	else if (*sign_cdt==0x00544443) return FW_TYPE_CDT;
+	else if (*sign_elf==0x464c457f) return FW_TYPE_ELF;
+	else if (*sign_mibib==0xfe569fac) return FW_TYPE_MIBIB;
+	else return -1;
 	return 0;
 }
